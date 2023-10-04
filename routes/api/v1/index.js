aa5083ef883456a33e072f9b893ee761907c35cf
Felipe Baro Tayahua <fbarotay@iu.edu>
2023-10-04 14:47:16 -0400

    End of v.6

---
+++ routes/api/v1/index.js
@@ -0,0 +1,47 @@
+const express = require('express');
+const router = express.Router();
+const mongoose = require('mongoose');
+require('models/Quote');
+const Quote = mongoose.model('quotes');
+
+router.get('/', (req,res) => {
+  res.send('Hi')
+});
+
+//Allow user to retrieve JSON format
+router.get('/quotes', async (req,res) => {
+  const filter ={};
+  const quotes = await Quote.find(filter);
+  console.log(quotes);
+  res.json(quotes);
+});
+
+// Allow user to retrieve all quotes for a single character 
+router.get('/quotes/character/:character', async (req,res) => {
+  character = req.params.character;
+  // const filter ={ "character": character};
+  // const quotes = await Quote.find(filter);
+  // console.log(quotes);
+  // res.json(quotes);
+
+  const regExpression = new RegExp(character, 'i');
+  const regexFilter ={ "character": { $regex: regExpression}};
+  const quotes = await Quote.find(regexFilter);
+  console.log(quotes);
+  res.json(quotes);
+
+});
+
+
+//v1/characters   This is to show every single character
+router.get('/characters', async (req,res)=> {
+  const characters = await Quote.distinct('character');
+  res.json(characters);
+})
+
+
+
+
+
+
+module.exports = router;
