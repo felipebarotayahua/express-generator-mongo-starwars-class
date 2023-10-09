a26ed3b922142ce911f45e88e23c77914a209fc7
Felipe Baro Tayahua <fbarotay@iu.edu>
2023-10-09 14:49:23 -0400

    swagger added

---
+++ services/swagger.js
@@ -0,0 +1,18 @@
+const swaggerAutogen = require('swagger-autogen')();
+
+const outputFile = './swagger_output.json';
+const endpointsFiles = ['./routes/api/v1/index.js'];
+
+const doc = {
+  info: {
+    version: "1.0.0",
+    title: "star wars quote",
+    description: "Quote of star wars characters through our API"
+  },
+  host: "localhost:3000",
+  basePath: "/api/v1",
+  schemes: ['http', 'https'],
+  consumes: ['application/json'],
+  produces: ['application/json']
+};
+swaggerAutogen(outputFile, endpointsFiles, doc)
